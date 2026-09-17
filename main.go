04250cd7d24b7de96b5c001bc75f84424111bd4e
Aryan More <aryanmore7210@email.com>
2026-09-17 21:25:33 +0530

    Add TTL expiration support to SET and GET commands

--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"fmt"
 	"net"
+	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
+
+// item holds the value and an optional expiration time
+
+type Item struct {
+	value     string
+	expiresAt time.Time // zero time means no expiration
+}
 
 //  in-memory store and a mutex to make it thread-safe for concurrent clients
 
 var (
-	kvStore = make(map[string]string)
+	kvStore = make(map[string]Item)
 	mu      sync.RWMutex
 )
 
@@ -83,9 +92,23 @@ func handleClient(conn net.Conn) {
 			if len(parts) > 3 {
 				key := parts[1]
 				val := parts[2]
+				var expiresAt time.Time
+
+				// check if an expiration argument was passed (eg SET key val EX 20 )
+
+				if len(parts) >= 5 && strings.ToUpper(parts[3]) == "EX" {
+					seconds, err := strconv.Atoi(parts[4])
+					if err == nil {
+						expiresAt = time.Now().Add(time.Duration(seconds) * time.Second)
+					}
+
+				}
 
 				mu.Lock()
-				kvStore[key] = val
+				kvStore[key] = Item{
+					value:     val,
+					expiresAt: expiresAt,
+				}
 				mu.Unlock()
 
 				_, _ = conn.Write([]byte("+OK\r\n"))
@@ -97,22 +120,36 @@ func handleClient(conn net.Conn) {
 			if len(parts) >= 2 {
 				key := parts[1]
 
-				mu.RLock()
-				val, exists := kvStore[key]
-				mu.RUnlock()
+				mu.Lock()
+				item, exists := kvStore[key]
 
-				if exists {
-					_, _ = conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(val), val)))
-				} else {
-					_, _ = conn.Write([]byte("-$-1\r\n")) // redis null bulk string or missing keys
+				// check if the key exists AND if it has expired
+
+				if !exists {
+					mu.Unlock()
+					_, _ = conn.Write([]byte("-$-1\r\n"))
+					break
 				}
+
+				if !item.expiresAt.IsZero() && time.Now().After(item.expiresAt) {
+
+					// its expired!! delete it from the map and treat it as missing
+
+					delete(kvStore, key)
+					mu.Unlock()
+					_, _ = conn.Write([]byte("$-1\r\n"))
+					break
+				}
+
+				val := item.value
+				mu.Unlock()
+				_, _ = conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(val), val)))
 			} else {
 				_, _ = conn.Write([]byte("-ERR wrong number of arguments for 'GET'\r\n"))
 			}
-
 		default:
 			_, _ = conn.Write([]byte("-ERR unknown command\r\n"))
+
 		}
 	}
-
 }
